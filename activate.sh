38701d7c310009d0018f659ee9d69b1d74897500
Abdullah Batiran <abady1000@gmail.com>
2026-06-03 13:28:50 +0300

    Add environment activation to another PC

--- activate.sh
+++ activate.sh
@@ -1,1 +1,7 @@
-source /home/ambat/opt/xilinx/2025.2/Vivado/settings64.sh
+# To support your own PC add a new line below
+
+source /home/ambat/opt/xilinx/2025.2/Vivado/settings64.sh 2> /dev/null || 
+source /home/ambat/opt/Xilinx/Vivado/2018.2/settings64.sh 2> /dev/null ||
+echo "No valid 'settings64.sh' file was found!"
+
+
